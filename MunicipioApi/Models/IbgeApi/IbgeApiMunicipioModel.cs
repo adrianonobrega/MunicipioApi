54f5f9aff81a60ed94ea2bd77cd61664f8e3a12f
adrianonobrega26 <adrianonobrega26@gmail.com>
2025-10-20 18:34:53 -0300

    feat: Implementa IbgeApiProvider e configura chaveamento de provedor via variaveis de ambiente

---
+++ MunicipioApi/Models/IbgeApi/IbgeApiMunicipioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MunicipioApi.Api.Models.IbgeApi
+{
+    public class IbgeApiMunicipioModel
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+
+    }
+}
